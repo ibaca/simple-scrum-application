aa5cc9bdd29cc7376edcda81bc82905dad9a5576
JuaNaN20 <JuaNaN20@51510980-78fc-55cd-15df-ff6b2c04aa8e>
2012-02-15 18:00:18 +0000

    git-svn-id: http://inftel-scrum-grupo-uno.googlecode.com/svn/trunk@31 51510980-78fc-55cd-15df-ff6b2c04aa8e

---
+++ ssa-web/src/main/java/org/inftel/ssa/web/session/SessionBean.java
@@ -0,0 +1,100 @@
+package org.inftel.ssa.web.session;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author JuaNaN
+ */
+@ManagedBean(name = "SessionBean")
+@RequestScoped
+public class SessionBean {
+
+    private String firstname = "";
+    private String surname = "";
+    private String email = "";
+    private String company = "";
+    private String password = "";
+    private String address = "";
+    private String city = "";
+    private String cp = "";
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    
+    public void login(){
+        
+    }
+    
+    public void signIn(){
+        
+    }
+    
+}
