8e0629e5183ad769d4377e73661dfb1938f85dd8
JuaNaN20 <JuaNaN20@51510980-78fc-55cd-15df-ff6b2c04aa8e>
2012-02-16 16:59:05 +0000

    Estadísticas con PrimeFaces
    git-svn-id: http://inftel-scrum-grupo-uno.googlecode.com/svn/trunk@51 51510980-78fc-55cd-15df-ff6b2c04aa8e

--- ssa-web/src/main/java/org/inftel/ssa/web/estadisticas/EstadisBean.java
+++ ssa-web/src/main/java/org/inftel/ssa/web/estadisticas/EstadisBean.java
@@ -1,8 +1,16 @@
 package org.inftel.ssa.web.estadisticas;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Map;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import org.inftel.ssa.datamining.DataminingDataPeriod;
+import org.inftel.ssa.datamining.DataminingProcessor;
 import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.LineChartSeries;
 
@@ -10,6 +18,8 @@ import org.primefaces.model.chart.LineChartSeries;
 @RequestScoped
 public class EstadisBean implements Serializable {
 
+    @EJB
+    private DataminingProcessor datamining;
     private CartesianChartModel esfuerzoModel;
     private CartesianChartModel tareasModel;
     private CartesianChartModel individualModel;
@@ -34,87 +44,61 @@ public class EstadisBean implements Serializable {
 
     private void createEsfuerzoModel() {
         esfuerzoModel = new CartesianChartModel();
+        LineChartSeries series = new LineChartSeries();
+        Map<Date, Long> samples; // todos los datos por fecha
 
-        LineChartSeries series1 = new LineChartSeries();
-        series1.setMarkerStyle("diamond");
-        series1.setLabel("Horas Pendientes");
-        series1.set(1, 250);
-        series1.set(2, 228);
-        series1.set(3, 140);
-        series1.set(4, 133);
-        series1.set(5, 120);
-        series1.set(6, 60);
-        series1.set(7, 40);
-        series1.set(8, 30);
-        series1.set(9, 0);
+        String name = "task." + "id_usuario" + "." + "sprint" + ".remaining";
+        samples = datamining.findStatistics(name, DataminingDataPeriod.DAYLY, new Date(0), new Date());
+        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, new Locale("es"));
 
-        LineChartSeries series2 = new LineChartSeries();
-        series2.setLabel("Ideal");
-        series2.setMarkerStyle(null);
-        series2.set(1, 250);
-        series2.set(9, 0);
+        for (Date date : samples.keySet()) {
+            series.set(df.format(date), samples.get(date));
+        }
 
-        esfuerzoModel.addSeries(series1);
-        esfuerzoModel.addSeries(series2);
+        esfuerzoModel.addSeries(series);
+
     }
 
     private void createTareasModel() {
+
         tareasModel = new CartesianChartModel();
+        LineChartSeries series = new LineChartSeries();
+        Map<Date, Long> samples; // todos los datos por fecha
 
-        LineChartSeries series1 = new LineChartSeries();
-        series1.setLabel("Tareas Pendientes");
-        series1.set(1, 25);
-        series1.set(2, 22);
-        series1.set(3, 14);
-        series1.set(4, 13);
-        series1.set(5, 12);
-        series1.set(6, 6);
-        series1.set(7, 4);
-        series1.set(8, 2);
-        series1.set(9, 0);
+        String name = "task." + "id_usuario" + "." + "sprint" + ".remaining";
+        samples = datamining.findStatistics(name, DataminingDataPeriod.DAYLY, new Date(0), new Date());
+        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, new Locale("es"));
 
-        tareasModel.addSeries(series1);
+        for (Date date : samples.keySet()) {
+            series.set(df.format(date), samples.get(date));
+        }
+
+        tareasModel.addSeries(series);
+
     }
 
     private void createIndividualModel() {
+
         individualModel = new CartesianChartModel();
+        List<User> listUsers = TaskFacade.findUsersBySprint(idSprint);  //Con idSprint el id del Sprint
+        Iterator iter = listUsers.iterator();
+        while (iter.hasNext()) {
 
-        LineChartSeries series1 = new LineChartSeries();
-        series1.setLabel("Ignacio");
-        series1.set(1, 100);
-        series1.set(2, 80);
-        series1.set(3, 50);
-        series1.set(4, 30);
-        series1.set(5, 0);
 
-        LineChartSeries series2 = new LineChartSeries();
-        series2.setLabel("Jesús Barriga");
-        series2.set(1, 100);
-        series2.set(2, 70);
-        series2.set(3, 60);
-        series2.set(4, 25);
-        series2.set(5, 0);
+            LineChartSeries series = new LineChartSeries();
+            series.setLabel(iter.name); //Con nameUser el nombre del Usuario
 
-        LineChartSeries series3 = new LineChartSeries();
-        series3.setLabel("Jesús");
-        series3.set(1, 100);
-        series3.set(2, 90);
-        series3.set(3, 80);
-        series3.set(4, 50);
-        series3.set(5, 0);
 
-        LineChartSeries series4 = new LineChartSeries();
-        series4.setLabel("Juan Antonio");
-        series4.set(1, 100);
-        series4.set(2, 70);
-        series4.set(3, 40);
-        series4.set(4, 10);
-        series4.set(5, 0);
+            Map<Date, Long> samples; // todos los datos por fecha
+            String name = "task." + "id_usuario" + "." + "sprint" + ".remaining";
+            samples = datamining.findStatistics(name, DataminingDataPeriod.DAYLY, new Date(0), new Date());
+            DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, new Locale("es"));
 
-        individualModel.addSeries(series1);
-        individualModel.addSeries(series2);
-        individualModel.addSeries(series3);
-        individualModel.addSeries(series4);
+            for (Date date : samples.keySet()) {
+                series.set(df.format(date), samples.get(date));
+            }
 
+            individualModel.addSeries(series);
+        }
     }
 }
