801c6c7cea2d77231da3c91f0c65b0ad1a0b6985
JuaNaN20 <JuaNaN20@51510980-78fc-55cd-15df-ff6b2c04aa8e>
2012-02-22 01:44:01 +0000

    Añadir ficheros de I18N
    git-svn-id: http://inftel-scrum-grupo-uno.googlecode.com/svn/trunk@129 51510980-78fc-55cd-15df-ff6b2c04aa8e

--- ssa-web/src/main/java/org/inftel/ssa/web/messages/LanguageBean.java
+++ ssa-web/src/main/java/org/inftel/ssa/web/messages/LanguageBean.java
@@ -1,21 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package org.inftel.ssa.web.languages;
+
+package org.inftel.ssa.web.messages;
 
 import javax.ejb.Stateless;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-/**
- *
- * @author JuaNaN
- */
+
 @Stateless
 @ManagedBean
 @RequestScoped
-public class languageBean {
+public class LanguageBean {
 
   private String locale="es";
 
