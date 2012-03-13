9df4c71b8f245ad5c45e7489f0747e8ac46baa13
JuaNaN20@gmail.com <JuaNaN20@gmail.com@51510980-78fc-55cd-15df-ff6b2c04aa8e>
2012-03-13 20:24:45 +0000

    Pantalla principal de usuario y detalles del mismo.
    git-svn-id: http://inftel-scrum-grupo-uno.googlecode.com/svn/trunk@323 51510980-78fc-55cd-15df-ff6b2c04aa8e

--- ssa-mobile/src/org/inftel/ssa/mobile/ui/fragments/UserDetailFragment.java
+++ ssa-mobile/src/org/inftel/ssa/mobile/ui/fragments/UserDetailFragment.java
@@ -1,5 +1,80 @@
+
 package org.inftel.ssa.mobile.ui.fragments;
 
-public class UserDetailFragment {
+import org.inftel.ssa.mobile.R;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class UserDetailFragment extends Activity {
+
+    private TextView fullname;
+    private TextView nickname;
+    private TextView email;
+    private TextView company;
+    private TextView role;
+    private TextView number;
+
+    private Button btnEmail;
+    private Button btnCall;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ssa_user_details);
+        fullname = (TextView) (findViewById(R.id.textView2));
+        nickname = (TextView) (findViewById(R.id.TextView3));
+        email = (TextView) (findViewById(R.id.TextView5));
+        company = (TextView) (findViewById(R.id.TextView7));
+        role = (TextView) (findViewById(R.id.TextView10));
+        number = (TextView) (findViewById(R.id.TextView07));
+        final Bundle extras = getIntent().getExtras();
+
+        if (extras == null) {
+            return;
+        }
+        fullname.setText(extras.getString("FullName"));
+        nickname.setText(extras.getString("NickName"));
+        email.setText(extras.getString("Email"));
+        company.setText(extras.getString("Company"));
+        role.setText(extras.getString("Role"));
+        number.setText(extras.getString("Number"));
+
+        btnEmail = (Button) (findViewById(R.id.button1));
+        btnEmail.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("text/plain");
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Asunto");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Su email aquí");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {
+                        extras.getString("Email")
+                });
+                startActivity(emailIntent);
+            }
+
+        });
+
+        btnCall = (Button) (findViewById(R.id.button2));
+        btnCall.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+
+                callIntent.setData(Uri.parse("tel:" + extras.getString("Number")));
+                startActivity(callIntent);
+
+            }
+
+        });
+
+    }
 }
--- ssa-mobile/src/org/inftel/ssa/mobile/ui/fragments/UserListFragment.java
+++ ssa-mobile/src/org/inftel/ssa/mobile/ui/fragments/UserListFragment.java
@@ -1,5 +1,224 @@
+
 package org.inftel.ssa.mobile.ui.fragments;
 
-public class UserListFragment {
+import java.io.Serializable;
+import java.util.ArrayList;
 
+import org.inftel.ssa.mobile.R;
+import org.inftel.ssa.mobile.contentproviders.UserContentProvider;
+import org.inftel.ssa.mobile.contentproviders.UserTable;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class UserListFragment extends Activity {
+
+    private UserData users[] = new UserData[50];
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ssa_user_list);
+        ContentResolver cr = getContentResolver();
+        ContentValues values = new ContentValues();
+
+        cr.delete(UserContentProvider.CONTENT_URI,
+                UserTable.KEY_NICKNAME + " = 'ibaca'", null);
+        cr.delete(UserContentProvider.CONTENT_URI,
+                UserTable.KEY_NICKNAME + " = 'JuaNaN'", null);
+        cr.delete(UserContentProvider.CONTENT_URI,
+                UserTable.KEY_NICKNAME + " = '3'", null);
+        cr.delete(UserContentProvider.CONTENT_URI,
+                UserTable.KEY_NICKNAME + " = '4'", null);
+
+        values.put(UserTable.KEY_FULLNAME, "Ignacio Baca");
+        values.put(UserTable.KEY_NICKNAME, "ibaca");
+        values.put(UserTable.KEY_EMAIL, "ignacio@gmail.com");
+        values.put(UserTable.KEY_NUMBER, "957700652");
+        values.put(UserTable.KEY_COMPANY, "Master Inftel");
+        values.put(UserTable.KEY_PASS, "inftel");
+        values.put(UserTable.KEY_ROLE, "SM");
+
+        cr.insert(UserContentProvider.CONTENT_URI, values);
+
+        values.put(UserTable.KEY_FULLNAME, "Juan Ant. Cobo");
+        values.put(UserTable.KEY_NICKNAME, "JuaNaN");
+        values.put(UserTable.KEY_EMAIL, "juanan20@gmail.com");
+        values.put(UserTable.KEY_NUMBER, "957700652");
+        values.put(UserTable.KEY_COMPANY, "Master Inftel");
+        values.put(UserTable.KEY_PASS, "inftel");
+        values.put(UserTable.KEY_ROLE, "SM");
+
+        cr.insert(UserContentProvider.CONTENT_URI, values);
+
+        values.put(UserTable.KEY_FULLNAME, "Jesus Ruiz");
+        values.put(UserTable.KEY_NICKNAME, "3");
+        values.put(UserTable.KEY_EMAIL, "jrovillano@gmail.com");
+        values.put(UserTable.KEY_NUMBER, "957700652");
+        values.put(UserTable.KEY_COMPANY, "Master Inftel");
+        values.put(UserTable.KEY_PASS, "inftel");
+        values.put(UserTable.KEY_ROLE, "SM");
+
+        cr.insert(UserContentProvider.CONTENT_URI, values);
+
+        values.put(UserTable.KEY_FULLNAME, "Jesus Barriga");
+        values.put(UserTable.KEY_NICKNAME, "4");
+        values.put(UserTable.KEY_EMAIL, "jesusbarriga@gmail.com");
+        values.put(UserTable.KEY_NUMBER, "957700652");
+        values.put(UserTable.KEY_COMPANY, "Master Inftel");
+        values.put(UserTable.KEY_PASS, "inftel");
+        values.put(UserTable.KEY_ROLE, "SM");
+
+        cr.insert(UserContentProvider.CONTENT_URI, values);
+
+        // Columnas de la tabla a recuperar
+        String[] projection = new String[] {
+                UserTable.KEY_ID,
+                UserTable.KEY_FULLNAME, UserTable.KEY_NICKNAME,
+                UserTable.KEY_EMAIL, UserTable.KEY_NUMBER,
+                UserTable.KEY_COMPANY,
+                UserTable.KEY_ROLE
+        };
+
+        Uri clientesUri = UserContentProvider.CONTENT_URI;
+
+        Cursor cur = cr.query(clientesUri, projection, // Columnas a devolver
+                null, // Condicion de la query
+                null, // Argumentos variables de la query
+                null); // Orden de los resultados
+
+        ArrayList<String> listausuarios = new ArrayList<String>();
+
+        if (cur.moveToFirst()) {
+            int colName = cur.getColumnIndex(UserTable.KEY_FULLNAME);
+            int colNickname = cur.getColumnIndex(UserTable.KEY_NICKNAME);
+            int colEmail = cur.getColumnIndex(UserTable.KEY_EMAIL);
+            int colNumber = cur.getColumnIndex(UserTable.KEY_NUMBER);
+            int colCompany = cur.getColumnIndex(UserTable.KEY_COMPANY);
+            int colRole = cur.getColumnIndex(UserTable.KEY_ROLE);
+            int i = 0;
+
+            do {
+                users[i] = new UserData();
+
+                users[i].setFullName(cur.getString(colName));
+                users[i].setNickName(cur.getString(colNickname));
+                users[i].setEmail(cur.getString(colEmail));
+                users[i].setNumber(cur.getString(colNumber));
+                users[i].setCompany(cur.getString(colCompany));
+                users[i].setRole(cur.getString(colRole));
+
+                listausuarios.add(users[i].getFullName());
+                i++;
+            } while (cur.moveToNext());
+        }
+
+        ListView userslist = (ListView) findViewById(R.id.UsersList);
+        listausuarios.toArray();
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listausuarios);
+        userslist.setAdapter(adaptador);
+
+        userslist.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> padre, View v, int posicion,
+                    long id) {
+                Intent details = new Intent(getApplicationContext(), UserDetailFragment.class);
+                int pos = (int) padre.getItemIdAtPosition(posicion);
+                details.putExtra("FullName", users[pos].getFullName());
+                details.putExtra("NickName", users[pos].getNickName());
+                details.putExtra("Email", users[pos].getEmail());
+                details.putExtra("Number", users[pos].getNumber());
+                details.putExtra("Company", users[pos].getCompany());
+                details.putExtra("Role", users[pos].getRole());
+
+                startActivity(details);
+                Toast.makeText(v.getContext(),
+                        padre.getItemAtPosition(posicion).toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    static class UserData implements Serializable {
+        private String fullName;
+        private String nickName;
+        private String email;
+        private String number;
+        private String company;
+        private String role;
+        private int id;
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public void setFullName(String fullName) {
+            this.fullName = fullName;
+        }
+
+        public String getNickName() {
+            return nickName;
+        }
+
+        public void setNickName(String nickName) {
+            this.nickName = nickName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getCompany() {
+            return company;
+        }
+
+        public void setCompany(String company) {
+            this.company = company;
+        }
+
+        public String getRole() {
+            return role;
+        }
+
+        public void setRole(String role) {
+            this.role = role;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+
+        public void setNumber(String number) {
+            this.number = number;
+        }
+
+    }
 }
