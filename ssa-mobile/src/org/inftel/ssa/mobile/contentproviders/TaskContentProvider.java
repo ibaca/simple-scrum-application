810e65d4f15e1a6976e41aeea087070d795c5e8c
jsbaes@gmail.com <jsbaes@gmail.com@51510980-78fc-55cd-15df-ff6b2c04aa8e>
2012-03-14 01:14:42 +0000

    Versión inicial clases TaskContentProvider, TaskDatabaseHelper y TaskTable.
    git-svn-id: http://inftel-scrum-grupo-uno.googlecode.com/svn/trunk@328 51510980-78fc-55cd-15df-ff6b2c04aa8e

--- ssa-mobile/src/org/inftel/ssa/mobile/contentproviders/TaskContentProvider.java
+++ ssa-mobile/src/org/inftel/ssa/mobile/contentproviders/TaskContentProvider.java
@@ -1,48 +1,213 @@
 
 package org.inftel.ssa.mobile.contentproviders;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.TextUtils;
 
 public class TaskContentProvider extends ContentProvider {
 
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // TODO Auto-generated method stub
-        return 0;
+    // database
+    private TaskDatabaseHelper database;
+
+    // Create the constants used to differentiate between the different URI
+    // requests.Used for the UriMacher
+    private static final int TASK = 10;
+    private static final int TASK_ID = 20;
+
+    private static final String AUTHORITY = "org.inftel.ssa.mobile.provider.task";
+
+    private static final String BASE_PATH = "task";
+
+    public static final Uri CONTENT_URI = Uri
+            .parse("content://" + AUTHORITY + "/" + BASE_PATH);
+
+    public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE
+            + "/task";
+    public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE
+            + "/task";
+
+    private static final UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sURIMatcher.addURI(AUTHORITY, BASE_PATH, TASK);
+        sURIMatcher.addURI(AUTHORITY, BASE_PATH + "/#", TASK_ID);
     }
 
-    @Override
-    public String getType(Uri uri) {
-        // TODO Auto-generated method stub
-        return null;
+    public TaskContentProvider() {
+        // TODO Auto-generated constructor stub
     }
 
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    // The default sort order for this table
+    public static final String DEFAULT_SORT_ORDER = "priority DESC, beginDate DESC";
 
     @Override
     public boolean onCreate() {
-        // TODO Auto-generated method stub
+        database = new TaskDatabaseHelper(getContext());
         return false;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
             String sortOrder) {
-        // TODO Auto-generated method stub
+
+        // Using SQLiteQueryBuilder instead of query() method
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        // Check if the caller has requested a column which does not exists
+        checkColumns(projection);
+
+        // Set the table
+        qb.setTables(TaskTable.TABLE_TASK);
+
+        // If this is a row query, limit the result set to the passed in row.
+        switch (sURIMatcher.match(uri)) {
+            case TASK:
+                break;
+            case TASK_ID:
+                qb.appendWhere(TaskTable.COLUMN_ID + "=" + uri.getPathSegments());
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+
+        String orderBy = null;
+        if (!TextUtils.isEmpty(sortOrder)) {
+            orderBy = sortOrder;
+        }
+
+        SQLiteDatabase db = database.getWritableDatabase();
+        Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, orderBy);
+
+        // Register the contexts ContentResolver to be notified if
+        // the cursor result set changes.
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // Return a cursor to the query result.
+        return c;
+    }
+
+    @Override
+    public String getType(Uri uri) {
         return null;
     }
 
     @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        // TODO Auto-generated method stub
-        return 0;
+    public Uri insert(Uri uri, ContentValues values) {
+        int uriType = sURIMatcher.match(uri);
+        SQLiteDatabase sqlDB = database.getWritableDatabase();
+        int rowsDeleted = 0;
+        long id = 0;
+        switch (uriType) {
+            case TASK:
+                id = sqlDB.insert(TaskTable.TABLE_TASK, null, values);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return Uri.parse(BASE_PATH + "/" + id);
     }
 
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int uriType = sURIMatcher.match(uri);
+        SQLiteDatabase sqlDB = database.getWritableDatabase();
+        int rowsDeleted = 0;
+        switch (uriType) {
+            case TASK:
+                rowsDeleted = sqlDB.delete(TaskTable.TABLE_TASK, selection,
+                        selectionArgs);
+                break;
+            case TASK_ID:
+                String id = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    rowsDeleted = sqlDB.delete(
+                            TaskTable.TABLE_TASK,
+                            TaskTable.COLUMN_ID + "=" + id,
+                            null);
+                } else {
+                    rowsDeleted = sqlDB.delete(
+                            TaskTable.TABLE_TASK,
+                            TaskTable.COLUMN_ID + "=" + id
+                                    + " and " + selection,
+                            selectionArgs);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return rowsDeleted;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+            String[] selectionArgs) {
+
+        int uriType = sURIMatcher.match(uri);
+        SQLiteDatabase sqlDB = database.getWritableDatabase();
+        int rowsUpdated = 0;
+        switch (uriType) {
+            case TASK:
+                rowsUpdated = sqlDB.update(TaskTable.TABLE_TASK,
+                        values,
+                        selection,
+                        selectionArgs);
+                break;
+            case TASK_ID:
+                String id = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    rowsUpdated = sqlDB.update(TaskTable.TABLE_TASK,
+                            values,
+                            TaskTable.COLUMN_ID + "=" + id,
+                            null);
+                } else {
+                    rowsUpdated = sqlDB.update(TaskTable.TABLE_TASK,
+                            values,
+                            TaskTable.COLUMN_ID + "=" + id
+                                    + " and "
+                                    + selection,
+                            selectionArgs);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return rowsUpdated;
+    }
+
+    private void checkColumns(String[] projection) {
+        String[] available = {
+                TaskTable.COLUMN_DESCRIPTION,
+                TaskTable.COLUMN_SUMMARY, TaskTable.COLUMN_ESTIMATED,
+                TaskTable.COLUMN_BURNED, TaskTable.COLUMN_REMAINING,
+                TaskTable.COLUMN_PRIORITY, TaskTable.COLUMN_BEGINDATE,
+                TaskTable.COLUMN_ENDDATE, TaskTable.COLUMN_STATUS,
+                TaskTable.COLUMN_USER, TaskTable.COLUMN_SPRINT,
+                TaskTable.COLUMN_PROJECT, TaskTable.COLUMN_COMMENTS,
+                TaskTable.COLUMN_CREATED, TaskTable.COLUMN_ID
+        };
+        if (projection != null) {
+            HashSet<String> requestedColumns = new HashSet<String>(
+                    Arrays.asList(projection));
+            HashSet<String> availableColumns = new HashSet<String>(
+                    Arrays.asList(available));
+            // Check if all columns which are requested are available
+            if (!availableColumns.containsAll(requestedColumns)) {
+                throw new IllegalArgumentException(
+                        "Unknown columns in projection");
+            }
+        }
+    }
 }
--- ssa-mobile/src/org/inftel/ssa/mobile/contentproviders/TaskTable.java
+++ ssa-mobile/src/org/inftel/ssa/mobile/contentproviders/TaskTable.java
@@ -1,5 +1,58 @@
+
 package org.inftel.ssa.mobile.contentproviders;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 public class TaskTable {
 
+    // Database table
+    public static final String TABLE_TASK = "task";
+
+    // Column Names
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_DESCRIPTION = "description";
+    public static final String COLUMN_SUMMARY = "summary";
+    public static final String COLUMN_ESTIMATED = "estimated";
+    public static final String COLUMN_BURNED = "burned";
+    public static final String COLUMN_REMAINING = "remaining";
+    public static final String COLUMN_PRIORITY = "priority";
+    public static final String COLUMN_BEGINDATE = "beginDate";
+    public static final String COLUMN_ENDDATE = "enddate";
+    public static final String COLUMN_STATUS = "status";
+    public static final String COLUMN_USER = "user";
+    public static final String COLUMN_SPRINT = "sprint";
+    public static final String COLUMN_PROJECT = "project";
+    public static final String COLUMN_COMMENTS = "comments";
+    public static final String COLUMN_CREATED = "created";
+
+    // Database creation sql statement
+    private static final String DATABASE_CREATE =
+            "create table " + TABLE_TASK + " ("
+                    + COLUMN_ID + " INTEGER primary KEY autoincrement, "
+                    + COLUMN_DESCRIPTION + " TEXT, "
+                    + COLUMN_SUMMARY + " TEXT, "
+                    + COLUMN_ESTIMATED + " INTEGER, "
+                    + COLUMN_BURNED + " INTEGER, "
+                    + COLUMN_REMAINING + " INTEGER, "
+                    + COLUMN_PRIORITY + " INTEGER, "
+                    + COLUMN_BEGINDATE + " DATE, "
+                    + COLUMN_ENDDATE + " DATE, "
+                    + COLUMN_STATUS + " INTEGER, "
+                    + COLUMN_USER + " INTEGER, "
+                    + COLUMN_SPRINT + " INTEGER, "
+                    + COLUMN_PROJECT + " INTEGER, "
+                    + COLUMN_CREATED + " DATE, "
+                    + COLUMN_COMMENTS + " TEXT); ";
+
+    public static void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    public static void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(TaskDatabaseHelper.class.getName(), "Upgrading database from version "
+                + oldVersion + " to " + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
+        onCreate(db);
+    }
 }
