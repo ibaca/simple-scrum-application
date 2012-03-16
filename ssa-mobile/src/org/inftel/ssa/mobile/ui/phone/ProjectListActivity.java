e0807dfdf1a5043156486ca0cfc7da9d167fe774
jrovillano@gmail.com <jrovillano@gmail.com@51510980-78fc-55cd-15df-ff6b2c04aa8e>
2012-03-16 13:40:31 +0000

    Metodo temporal que rellena con datos ficticios la tabla de projects
    git-svn-id: http://inftel-scrum-grupo-uno.googlecode.com/svn/trunk@365 51510980-78fc-55cd-15df-ff6b2c04aa8e

--- ssa-mobile/src/org/inftel/ssa/mobile/ui/phone/ProjectListActivity.java
+++ ssa-mobile/src/org/inftel/ssa/mobile/ui/phone/ProjectListActivity.java
@@ -1,15 +1,21 @@
 
 package org.inftel.ssa.mobile.ui.phone;
 
+import org.inftel.ssa.mobile.contentproviders.ProjectContentProvider;
+import org.inftel.ssa.mobile.contentproviders.ProjectTable;
 import org.inftel.ssa.mobile.ui.BaseSinglePaneActivity;
 import org.inftel.ssa.mobile.ui.fragments.ProjectListFragment;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
 public class ProjectListActivity extends BaseSinglePaneActivity {
     @Override
     protected Fragment onCreatePane() {
+        insertProjectsDataTable();
         return new ProjectListFragment();
     }
 
@@ -18,4 +24,73 @@ public class ProjectListActivity extends BaseSinglePaneActivity {
         super.onPostCreate(savedInstanceState);
         getActivityHelper().setupSubActivity();
     }
+
+    private void insertProjectsDataTable() {
+        Log.d(getClass().getSimpleName(), "Insertando datos prueba");
+        ContentResolver cr = getContentResolver();
+        ContentValues values = new ContentValues();
+
+        cr.delete(ProjectContentProvider.CONTENT_URI,
+                ProjectTable.KEY_NAME + " = 'Proyecto 1'", null);
+        cr.delete(ProjectContentProvider.CONTENT_URI,
+                ProjectTable.KEY_NAME + " = 'Proyecto 2'", null);
+        cr.delete(ProjectContentProvider.CONTENT_URI,
+                ProjectTable.KEY_NAME + " = 'Proyecto 3'", null);
+        cr.delete(ProjectContentProvider.CONTENT_URI,
+                ProjectTable.KEY_NAME + " = 'Proyecto 4'", null);
+
+        values.put(ProjectTable.KEY_NAME, "Proyecto 1");
+        values.put(ProjectTable.KEY_SUMMARY, "Pasos");
+        values.put(ProjectTable.KEY_DESCRIPTION, "Gestion de alarmas para ancianos");
+        values.put(ProjectTable.KEY_OPENED, "12-3-2012");
+        values.put(ProjectTable.KEY_STARTED, "12-3-2012");
+        values.put(ProjectTable.KEY_CLOSE, "12-3-2012");
+        values.put(ProjectTable.KEY_COMPANY, "Inftel");
+        values.put(ProjectTable.KEY_LINKS, "www.inftel.com");
+        values.put(ProjectTable.KEY_LABELS, "");
+        values.put(ProjectTable.KEY_LICENSE, "GPL");
+
+        cr.insert(ProjectContentProvider.CONTENT_URI, values);
+
+        values.put(ProjectTable.KEY_NAME, "Proyecto 2");
+        values.put(ProjectTable.KEY_SUMMARY, "Centro Medico");
+        values.put(ProjectTable.KEY_DESCRIPTION, "Gestion del centro ambulatorio");
+        values.put(ProjectTable.KEY_OPENED, "12-3-2012");
+        values.put(ProjectTable.KEY_STARTED, "12-3-2012");
+        values.put(ProjectTable.KEY_CLOSE, "12-3-2012");
+        values.put(ProjectTable.KEY_COMPANY, "Inftel");
+        values.put(ProjectTable.KEY_LINKS, "www.inftel.com");
+        values.put(ProjectTable.KEY_LABELS, "");
+        values.put(ProjectTable.KEY_LICENSE, "GPL");
+
+        cr.insert(ProjectContentProvider.CONTENT_URI, values);
+
+        values.put(ProjectTable.KEY_NAME, "Proyecto 3");
+        values.put(ProjectTable.KEY_SUMMARY, "Centro de Reparaciones");
+        values.put(ProjectTable.KEY_DESCRIPTION, "Control de inventario");
+        values.put(ProjectTable.KEY_OPENED, "12-3-2012");
+        values.put(ProjectTable.KEY_STARTED, "12-3-2012");
+        values.put(ProjectTable.KEY_CLOSE, "12-3-2012");
+        values.put(ProjectTable.KEY_COMPANY, "Inftel");
+        values.put(ProjectTable.KEY_LINKS, "www.inftel.com");
+        values.put(ProjectTable.KEY_LABELS, "");
+        values.put(ProjectTable.KEY_LICENSE, "GPL");
+
+        cr.insert(ProjectContentProvider.CONTENT_URI, values);
+
+        values.put(ProjectTable.KEY_NAME, "Proyecto 4");
+        values.put(ProjectTable.KEY_SUMMARY, "Central Eolica");
+        values.put(ProjectTable.KEY_DESCRIPTION, "Gestion de recursos");
+        values.put(ProjectTable.KEY_OPENED, "12-3-2012");
+        values.put(ProjectTable.KEY_STARTED, "12-3-2012");
+        values.put(ProjectTable.KEY_CLOSE, "12-3-2012");
+        values.put(ProjectTable.KEY_COMPANY, "Inftel");
+        values.put(ProjectTable.KEY_LINKS, "www.inftel.com");
+        values.put(ProjectTable.KEY_LABELS, "");
+        values.put(ProjectTable.KEY_LICENSE, "GPL");
+
+        cr.insert(ProjectContentProvider.CONTENT_URI, values);
+        Log.d(getClass().getSimpleName(), "Fin de insercion de datos");
+    }
+
 }
