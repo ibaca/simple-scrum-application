3e37e7f27f50e4253c63421f4b79642023013337
jsbaes@gmail.com <jsbaes@gmail.com@51510980-78fc-55cd-15df-ff6b2c04aa8e>
2012-03-22 01:51:12 +0000

    Actualización de la lista de tareas
    git-svn-id: http://inftel-scrum-grupo-uno.googlecode.com/svn/trunk@433 51510980-78fc-55cd-15df-ff6b2c04aa8e

--- ssa-mobile/src/org/inftel/ssa/mobile/ui/phone/TaskListActivity.java
+++ ssa-mobile/src/org/inftel/ssa/mobile/ui/phone/TaskListActivity.java
@@ -1,13 +1,25 @@
 
 package org.inftel.ssa.mobile.ui.phone;
 
+import static android.content.Intent.ACTION_INSERT;
+
+import org.inftel.ssa.mobile.R;
+import org.inftel.ssa.mobile.provider.SsaContract.Projects;
+import org.inftel.ssa.mobile.provider.SsaContract.Tasks;
 import org.inftel.ssa.mobile.ui.BaseSinglePaneActivity;
 import org.inftel.ssa.mobile.ui.fragments.TaskListFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class TaskListActivity extends BaseSinglePaneActivity {
+	private static final String TAG = "TaskListActivity";
+	
     @Override
     protected Fragment onCreatePane() {
         return new TaskListFragment();
@@ -18,5 +30,22 @@ public class TaskListActivity extends BaseSinglePaneActivity {
         super.onPostCreate(savedInstanceState);
         getActivityHelper().setupSubActivity();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.ssa_task_list_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_add:
+                Log.d(TAG, "Creando nueva tarea");
+                startActivity(new Intent(ACTION_INSERT, Tasks.CONTENT_URI));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
