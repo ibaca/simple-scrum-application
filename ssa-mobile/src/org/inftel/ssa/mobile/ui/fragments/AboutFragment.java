099ee1ec07b07296ad6d650b35ba667c2548c8e1
jsbaes@gmail.com <jsbaes@gmail.com@51510980-78fc-55cd-15df-ff6b2c04aa8e>
2012-03-23 17:59:11 +0000

    Nuevo fragment para la ventana de About
    git-svn-id: http://inftel-scrum-grupo-uno.googlecode.com/svn/trunk@477 51510980-78fc-55cd-15df-ff6b2c04aa8e

---
+++ ssa-mobile/src/org/inftel/ssa/mobile/ui/fragments/AboutFragment.java
@@ -0,0 +1,27 @@
+
+package org.inftel.ssa.mobile.ui.fragments;
+
+import org.inftel.ssa.mobile.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AboutFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.about, container, false);
+
+        return view;
+    }
+}
